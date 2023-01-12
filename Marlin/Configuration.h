8416fb10d0cb9f06516ba9adc27ed1b68efe9fe5
phillip <phillip.hagerman@scrith.com>
2023-01-11 20:09:07 -0600

    Resetting BED_SIZE and home offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1485,7 +1485,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -7, -2.45 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -7, -2.15 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1696,8 +1696,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 235
+#define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
