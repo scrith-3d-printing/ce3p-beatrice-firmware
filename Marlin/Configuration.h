c9a77761f30e39a81932cb8ab0510699cc08d464
Phillip Hagerman <phagerma@redhat.com>
2023-01-25 14:56:58 -0600

    Default extruder PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -669,9 +669,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  27.5
+    #define DEFAULT_Ki   2.79
+    #define DEFAULT_Kd  67.5
   #endif
 #endif
 
